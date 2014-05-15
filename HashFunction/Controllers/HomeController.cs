570dabf999b0d5475145766fb6e02500979a9285
gavra90 <gavra90@gmail.com>
2014-05-15 08:06:49 -0700

    inicijalni commit - postavljanje projekta na git

---
+++ HashFunction/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using HashFunction.Models;
+using System.Threading;
+
+namespace HashFunction.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(String text)
+        {
+            String hash = PasswordHash.CreateHash("Gavra");
+            Thread.Sleep(2000);
+            ViewBag.hashf = PasswordHash.ValidatePassword(text, hash);
+            return View();
+        }
+
+    }
+}
